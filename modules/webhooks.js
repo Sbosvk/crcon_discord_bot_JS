35bca205f81731b8dbbb8d4c0387d233c66e9341
Oliver Krilov <sbosvk@gmail.com>
2024-08-25 14:06:11 +0200

    fix: stringify

--- modules/webhooks.js
+++ modules/webhooks.js
@@ -30,7 +30,7 @@ modulesConfig.modules.forEach((moduleConfig) => {
 
         // Handle POST requests for webhook usage
         app.post(`/webhook/${moduleName}`, (req, res) => {
-            console.log("webhooks", `Received payload on: ${moduleName}`, req.body);
+            console.log("webhooks", `Received payload on: ${moduleName}`, JSON.stringify(req.body));
             if (module && module.nativeWebhook) {
                 module.nativeWebhook(req.body, config);
             } else {
