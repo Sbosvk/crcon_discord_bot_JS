29ace5486a3ee2c7795265e102df24b10dbaf006
Oliver Krilov <sbosvk@gmail.com>
2025-01-07 21:27:00 +0100

    fix(custom_commands): Ensure usage of log stream objects

--- modules/custom_commands.js
+++ modules/custom_commands.js
@@ -20,23 +20,23 @@ module.exports = (client, pool, config) => {
             trigger: "commands",
             description: "List all available commands.",
             isClanOnly: false,
-            execute: async function (playerName, args, pool, config, webhook) {
+            execute: async function (log, pool, config) {
                 try {
-                    // Extract the player's Steam ID from the webhook
-                    const playerSteamID = extractSteamIDFromWebhook(webhook);
-        
+                    const playerSteamID = log.player_id_1;
+                    const playerName = log.player_name_1;
+
                     // Construct a message with all commands
                     const commandList = commands
                         .map((cmd) => `!${cmd.trigger}: ${cmd.description}`)
                         .join("\n\n");
-        
+
                     // Send the constructed message to the player
                     await api.message_player({
                         player_id: playerSteamID,
                         message: `Available Commands:\n${commandList}`,
                         by: messages_by(config)
                     });
-        
+
                     console.log(`🧩 Sent command list to ${playerName}`);
                 } catch (error) {
                     console.error(`🧩 Error executing custom command '${this.trigger}':`, error);
@@ -46,32 +46,32 @@ module.exports = (client, pool, config) => {
         //=================SWITCH TEAM=================
         {
             trigger: "change",
-            "description": "Change your team immediately. Only available to clan member.",
+            description: "Change your team immediately. Only available to clan member.",
             isClanOnly: true,
-            execute: async function (playerName, args, pool, config, webhook) {
+            execute: async function (log, pool, config) {
                 try {
+                    const playerSteamID = log.player_id_1;
+                    const playerName = log.player_name_1;
+        
+                    // Determine the current team from the `log.action` field
+                    const currentTeam = log.action.includes("Axis") ? "axis" : "allies";
+                    const oppositeTeam = currentTeam === "axis" ? "allies" : "axis";
+        
                     const detailedPlayers = await api.get_detailed_players();
                     const players = detailedPlayers.players;
-                    const playerSteamID = extractSteamIDFromWebhook(webhook);
                     const playerInfo = players[playerSteamID];
-
+        
                     if (!playerInfo) {
-                        console.warn("🧩", `Player ${playerName} not found in detailed players.`);
+                        console.warn(`🧩 Player ${playerName} not found in detailed players.`);
                         return;
                     }
-
-                    if (playerName.includes("[Allies][Team]")) playerName = playerName.split("[Allies]")[0].trim();
-                    else playerName = playerName.split("[Axis]")[0].trim();
-
-                    const currentTeam = playerInfo.team;
-                    const oppositeTeam = currentTeam === "axis" ? "allies" : "axis";
-
+        
                     const publicInfo = await api.get_public_info();
                     const maxPlayersPerTeam = publicInfo.max_player_count / 2;
                     const playerCounts = publicInfo.player_count_by_team;
-
+        
                     const teamFull = playerCounts[oppositeTeam] >= maxPlayersPerTeam;
-
+        
                     if (teamFull) {
                         await api.message_player({
                             player_name: playerName,
@@ -79,64 +79,63 @@ module.exports = (client, pool, config) => {
                             message: `Sorry ${playerName}, but the ${oppositeTeam} team is currently full.`,
                             by: messages_by(config)
                         });
-                        console.log("🧩", `Player ${playerName} attempted to switch to ${oppositeTeam}, but the team was full.`);
+                        console.log(`🧩 Player ${playerName} attempted to switch to ${oppositeTeam}, but the team was full.`);
                     } else {
                         const switchResult = await api.switch_player_now({
                             player_name: playerName,
                         });
-
+        
                         if (switchResult) {
-                            console.log("🧩", `Player ${playerName} switched to ${oppositeTeam} team successfully.`);
+                            console.log(`🧩 Player ${playerName} switched to ${oppositeTeam} team successfully.`);
                         } else {
-                            console.log("🧩", `Failed to switch ${playerName} to ${oppositeTeam}.`);
+                            console.log(`🧩 Failed to switch ${playerName} to ${oppositeTeam}.`);
                         }
                     }
                 } catch (error) {
-                    console.error(`🧩 Error executing custom command '${this.trigger}':`, error);
+                    console.error(`🧩 Error executing custom command 'change':`, error);
                 }
-            },
+            }
         },
         //=================STATS OPT-OUT=================
         {
             trigger: "stats",
-            "description": "Opt-out for death stats. Usage: '!stats on' or '!stats off'",
+            description: "Opt-out for death stats. Usage: '!stats on' or '!stats off'",
             isClanOnly: false,
-            execute: async function (playerName, args, pool, config, webhook) {
+            execute: async function (log, pool, config) {
                 try {
-                    const playerSteamID = extractSteamIDFromWebhook(webhook);
-                    if (!playerSteamID) {
-                        console.log("🧩", `Failed to extract SteamID for ${playerName}.`);
+                    const playerSteamID = log.player_id_1;
+                    const playerName = log.player_name_1;
+                    const [_, action] = log.sub_content.split(/\s+/); // Extract the action (e.g., "on", "off")
+
+                    if (!action) {
+                        console.log("🧩", `Invalid stats command arguments from ${playerName}.`);
                         return;
                     }
 
-                    if (args[0] === "on") {
-                        const query = `
-                            DELETE FROM player_preferences WHERE steamID = $1;
-                        `;
-                        await pool.query(query, [playerSteamID]);
+                    if (action === "on") {
+                        await pool.query(`DELETE FROM player_preferences WHERE steamID = $1;`, [playerSteamID]);
                         console.log("🧩", `${playerName} has opted in for death stats.`);
-                        api.message_player({
+                        await api.message_player({
                             player_id: playerSteamID,
                             message: "You have successfully opted IN for death stats updates.",
                             by: messages_by(config)
                         });
-                    } else if (args[0] === "off") {
-                        const query = `
+                    } else if (action === "off") {
+                        await pool.query(`
                             INSERT INTO player_preferences (steamID, optedOut)
                             VALUES ($1, TRUE)
                             ON CONFLICT (steamID)
                             DO UPDATE SET optedOut = TRUE;
-                        `;
-                        await pool.query(query, [playerSteamID]);
+                        `, [playerSteamID]);
                         console.log("🧩", `${playerName} has opted out of death stats.`);
-                        api.message_player({
+                        await api.message_player({
                             player_id: playerSteamID,
                             message: "You have successfully opted OUT of death stats updates.",
                             by: messages_by(config)
                         });
                     } else {
-                        console.log("🧩", `Invalid argument for stats command: ${args[0]}`);
-                        api.message_player({
+                        console.log("🧩", `Invalid argument for stats command: ${action}`);
+                        await api.message_player({
                             player_id: playerSteamID,
                             message: "Invalid command. Use `!stats on` to opt-in or `!stats off` to opt-out.",
                             by: messages_by(config)
@@ -145,34 +144,39 @@ module.exports = (client, pool, config) => {
                 } catch (error) {
                     console.error(`🧩 Error executing custom command '${this.trigger}':`, error);
                 }
-            },
+            }
         },
         //=================TIPS=================
         {
             trigger: "tip",
-            "description": "Get a random tip.",
+            description: "Get a random tip.",
             isClanOnly: false,
-            execute: async function (playerName, args, pool, config, webhook) {
+            execute: async function (log, pool, config) {
                 try {
                     const phrases = config.tips.phrases;
                     if (!phrases || phrases.length === 0) {
                         throw new Error("No tips phrases found in the config.");
                     }
-
-                    const playerSteamID = extractSteamIDFromWebhook(webhook);
-
+        
+                    const playerSteamID = log.player_id_1;
+                    const playerName = log.player_name_1;
+        
+                    // Select a random tip
                     const randomTip = phrases[Math.floor(Math.random() * phrases.length)];
-
-                    api.message_player({
+        
+                    // Send the tip to the player
+                    await api.message_player({
                         player_id: playerSteamID,
                         message: randomTip,
                         by: messages_by(config)
                     });
-                } catch(error) {
-                    console.error(`🧩 Error executing custom command '${this.trigger}':`, error);
+        
+                    console.log(`🧩 Sent a random tip to ${playerName} (SteamID: ${playerSteamID}).`);
+                } catch (error) {
+                    console.error(`🧩 Error executing custom command 'tip':`, error);
                 }
             }
-        }
+        }        
     ];
 
     const isClanMember = (playerName, config) => {
@@ -186,26 +190,26 @@ module.exports = (client, pool, config) => {
     };
 
     const processChatLog = async (log) => {
-        const commandCheck = isCommand(log);
+        try {
+            const commandCheck = isCommand(log);
+            if (!commandCheck.result) return; // Not a command, ignore
 
-        if (!commandCheck.result) return; // Not a command, ignore
+            const [commandTrigger] = commandCheck.content.split(/\s+/);
+            const command = commands.find((cmd) => cmd.trigger === commandTrigger);
 
-        const [commandTrigger, ...args] = commandCheck.content.split(/\s+/);
-        const command = commands.find((cmd) => cmd.trigger === commandTrigger);
+            if (command) {
+                const playerName = log.player_name_1 || "Unknown Player";
+                if (command.isClanOnly && !isClanMember(playerName, config)) {
+                    console.log(`🧩 Player ${playerName} is not a clan member. Command ignored.`);
+                    return;
+                }
 
-        if (command) {
-            const playerName = log.player_name_1 || "Unknown Player";
-            const playerSteamID = log.player_id_1 || "Unknown SteamID";
-            const team = extractTeam(log.action);
-
-            if (command.isClanOnly && !isClanMember(playerName, config)) {
-                console.log(`🧩 Player ${playerName} is not a clan member. Command ignored.`);
-                return;
+                await command.execute(log, pool, config); // Pass the full log object to the command
+            } else {
+                console.log(`🧩 Unknown command: ${commandTrigger}`);
             }
-
-            await command.execute(playerName, playerSteamID, team, args, pool, config);
-        } else {
-            console.log(`🧩 Unknown command: ${commandTrigger}`);
+        } catch (error) {
+            console.error("🧩 Error processing chat log:", error);
         }
     };
 
