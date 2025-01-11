5be022d7bffce04f2ef290a8f7fb2a559407cbb4
Oliver Krilov <sbosvk@gmail.com>
2025-01-11 02:40:19 +0100

    debug

--- modules/admin_threads.js
+++ modules/admin_threads.js
@@ -187,7 +187,7 @@ module.exports = async (client, pool, config) => {
     client.on("interactionCreate", async (interaction) => {
         if (!interaction.isButton()) return;
 
-        console.log(interaction);
+        console.log(JSON.stringify(interaction, null, 2));
 
         const [action, player_id] = interaction.customId.split("_");
         if (action === "close_thread") {
