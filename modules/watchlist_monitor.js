e5624517db7cb3a66a9bd0f080f11ca9ff3be9a7
Oliver Krilov <sbosvk@gmail.com>
2025-01-12 19:20:11 +0100

    fix(watchlist_monitor): Configurable notifications.
    Make discord/ingame notifications configurable in module config (true/false)
    Add some debugging statements

--- modules/watchlist_monitor.js
+++ modules/watchlist_monitor.js
@@ -11,35 +11,40 @@ const api = new API(CRCON_API_URL, { token: CRCON_API_TOKEN });
 
 // Process watchlist notification
 const processWatchlistNotification = async (client, config, player) => {
+    if (!config.discord_notification || !config.ingame_notification) return;
     try {
-        // Notify in-game admins
-        const onlineMods = await api.get_ingame_mods();
-        if (onlineMods && Array.isArray(onlineMods)) {
-            for (const mod of onlineMods) {
-                const message = `Watchlisted player ${player.player_name} is online.\n\nWatching reason: ${player.reason}`;
-                await api.message_player({
-                    player_id: mod.player_id,
-                    message: message,
-                    by: "Watchlist Monitor",
-                });
-                console.log("🧩", `Notified admin ${mod.player_name} about ${player.player_name}.`);
+        if (config.ingame_notification) {
+            // Notify in-game admins
+            const onlineMods = await api.get_ingame_mods();
+            if (onlineMods && Array.isArray(onlineMods)) {
+                for (const mod of onlineMods) {
+                    const message = `Watchlisted player ${player.player_name} is online.\n\nWatching reason: ${player.reason}`;
+                    await api.message_player({
+                        player_id: mod.player_id,
+                        message: message,
+                        by: "Watchlist Monitor",
+                    });
+                    console.log("🧩", `Notified admin ${mod.username} about ${player.player_name}.`);
+                }
+            } else {
+                console.error("🧩", "No online mods found or invalid data structure.");
             }
-        } else {
-            console.error("🧩", "No online mods found or invalid data structure.");
         }
 
-        // Notify Discord channel
-        const channel = await client.channels.fetch(config.channel_id);
-        if (channel) {
-            const embed = new EmbedBuilder()
-                .setTitle("🚨 Watchlisted Player Online")
-                .setDescription(
-                    `Player **${player.player_name}** is now online. [Steam Profile](https://steamcommunity.com/profiles/${player.player_id})\n\nWatching reason: ${player.reason}`
-                )
-                .setColor(0xff0000)
-                .setTimestamp();
-            await channel.send({ embeds: [embed] })
-                .then(() => console.log("🧩", "Sent watchlist notification"));
+        if (config.discord_notification) {
+            // Notify Discord channel
+            const channel = await client.channels.fetch(config.channel_id);
+            if (channel) {
+                const embed = new EmbedBuilder()
+                    .setTitle("🚨 Watchlisted Player Online")
+                    .setDescription(
+                        `Player **${player.player_name}** is now online. [Steam Profile](https://steamcommunity.com/profiles/${player.player_id})\n\nWatching reason: ${player.reason}`
+                    )
+                    .setColor(0xff0000)
+                    .setTimestamp();
+                await channel.send({ embeds: [embed] })
+                    .then(() => console.log("🧩", "Sent watchlist notification"));
+            }
         }
     } catch (error) {
         console.error("🧩", "Error notifying watchlisted player:", error);
@@ -53,8 +58,10 @@ module.exports = async (client, pool, config) => {
      logStreamManager.subscribe("CONNECTED");
  
      logStreamManager.on("CONNECTED", async (log) => {
+        console.log("connected log received:", log)
         let player = await isWatched(log);
         if (player.result) {
+            console.log("isWatched returned:", player)
             await processWatchlistNotification(client, config, player);
         }
      });
